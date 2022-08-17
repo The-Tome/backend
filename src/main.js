deb07502f20bcd1b216ccf2c56806e21c4292b67
StevenR123 <stevenreninga2@gmail.com>
2022-08-17 16:48:17 +0000

    Adde/Installed Sentry

--- src/main.js
+++ src/main.js
@@ -1,10 +1,34 @@
 const express = require('express')
+const Sentry = require('@sentry/node');
+const Tracing = require('@sentry/tracing');
+
 const app = express()
 const port = 3001
+
+Sentry.init({
+  dsn: "https://70eaaecafa784be9bda52042320bc8e5@o1365087.ingest.sentry.io/6660472",
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Tracing.Integrations.Express({ app }),
+  ],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
+app.use(Sentry.Handlers.requestHandler());
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler());
 
 app.get('/', (req, res) => {
   res.send('Hello Browser!')
 })
+
+app.use(Sentry.Handlers.errorHandler());
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
